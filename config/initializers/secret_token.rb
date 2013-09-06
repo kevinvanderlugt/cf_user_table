22f4463ea2f25d21f4d43a1d7ccd816997fcd600
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-05 20:35:56 -0700

    Initial commit with user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserList::Application.config.secret_key_base = '1a926d75af67594617a3bacaed8bbbed66b5675d8cdd3809c98cee2efd652f4c40db62fcb23a763f8ead998bd57bf0beaea1491d7826f6e1cb7a5a05845b6c3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UserList::Application.config.session_store :cookie_store, key: '_user_list_session'
