364d37cda39172686221854ebd0765447cf73932
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-07 12:02:12 -0700

    Refactors the new form into a form partial to use in new and edit actions on the UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+  end
+
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :phone, :email)
