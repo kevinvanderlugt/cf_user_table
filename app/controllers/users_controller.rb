8e49aa29d7355496c2919b4aaa6dc0b9b44ebbfb
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-07 10:08:19 -0700

    Adds ability to create new users via the new and create actions in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -12,6 +13,25 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User was added"
+      redirect_to @user
+    else
+      flash[:error] = "Unable to add user"
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :phone, :email)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
 end
