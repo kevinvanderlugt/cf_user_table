269c15ab6eb4f0bbe648a2fd04e5930e8bbe1081
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-07 09:57:01 -0700

    Adds users controller autogenerated functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 UserList::Application.routes.draw do
-  resources :user
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
